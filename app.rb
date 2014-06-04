025e16bb07e0fa424977148fddf60b9741c9df2a
Casey Kolderup <casey@kolderup.org>
2014-06-04 02:19:42 -0400

    actually allow any valid season

--- app.rb
+++ app.rb
@@ -13,12 +13,16 @@ end.parse!
 keys = File.readlines("tv_shows.txt")
 template = "%s in the streets, %s in the sheets"
 
+def pick_season(max)
+  (1..max.to_i).to_a.sample
+end
+
 length = 141
 while length > 140 do
     streetsraw = keys.sample.chomp.split(',')
     sheetsraw = keys.sample.chomp.split(',')
-    streets = "#{streetsraw.first} Season #{streetsraw.last}"
-    sheets = "#{sheetsraw.first} Season #{sheetsraw.last}"
+    streets = "#{streetsraw.first} Season #{pick_season(streetsraw.last)}"
+    sheets = "#{sheetsraw.first} Season #{pick_season(sheetsraw.last)}"
     length = (template.length - 4) + streets.length + sheets.length
 end
 
